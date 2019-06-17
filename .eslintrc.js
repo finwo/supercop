f86a2fa05392718a333f1169a4ee45fc32786d5d
Robin Bron <robin@finwo.nl>
2019-06-17 20:02:59 +0200

    Let's enforce style

---
+++ .eslintrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+    'env': {
+        'browser': true,
+        'commonjs': true,
+        'es6': true,
+        'node': true
+    },
+    'extends': 'eslint:recommended',
+    'globals': {
+        'Atomics': 'readonly',
+        'SharedArrayBuffer': 'readonly'
+    },
+    'parserOptions': {
+        'ecmaVersion': 2018,
+        'sourceType': 'module'
+    },
+  'rules': {
+        'indent': [
+            'error',
+            2,
+            {
+                'SwitchCase': 1,
+                'VariableDeclarator': 'first'
+            }
+        ],
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'always'
+        ],
+        'no-trailing-spaces': [
+            'error'
+        ],
+        'comma-dangle': [
+            'error',
+            'always-multiline'
+        ]
+    }
+};
