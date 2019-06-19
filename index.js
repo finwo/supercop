a44709b5372375d18a77524d3779f739ef59efd7
finwo <finwo@pm.me>
2019-06-19 12:30:26 +0200

    Helper functions are now tested as well

--- index.js
+++ index.js
@@ -5,14 +5,48 @@ function randomBytes(length) {
   return Buffer.from(new Array(length).fill(0).map(()=>Math.floor(Math.random()*256)));
 }
 
+function checkArguments( namedArguments, callback ) {
+  callback = callback || function( err ) {
+    if (!err) return;
+    if (err instanceof Error) throw err;
+    throw new Error(err);
+  };
+
+  if ('object' !== typeof namedArguments) return callback('Expected object, ' + (typeof namedArguments) + ' given');
+  if (!namedArguments) return callback('Expected object, null given');
+  if ( 'seed' in namedArguments ) {
+    if (!isBuffer(namedArguments.seed)   ) return callback('Seed is not a buffer');
+    if (namedArguments.seed.length !== 32) return callback('Seed must be 32 bytes');
+  }
+  if ( 'signature' in namedArguments ) {
+    if (!isBuffer(namedArguments.signature)) return callback('Signature is not a buffer');
+    if (namedArguments.signature.length !== 64) return callback('Signature must be 64 bytes');
+  }
+  if ( 'message' in namedArguments ) {
+    if (!isBuffer(namedArguments.message)) return callback('Message is not a buffer');
+  }
+  if ( 'publicKey' in namedArguments ) {
+    if (!isBuffer(namedArguments.publicKey)   ) return callback('Public key is not a buffer');
+    if (namedArguments.publicKey.length !== 32) return callback('Public key must be 32 bytes');
+  }
+  if ( 'secretKey' in namedArguments ) {
+    if (!isBuffer(namedArguments.secretKey)   ) return callback('Secret key is not a buffer');
+    if (namedArguments.secretKey.length !== 64) return callback('Secret key must be 64 bytes');
+  }
+
+  return callback();
+}
+
+// Export helpers
+exports._checkArguments = checkArguments;
+exports._randomBytes    = randomBytes;
+
 exports.createSeed = function(){
   return randomBytes(32);
 };
 
 exports.createKeyPair = function(seed) {
-  if(!Buffer.isBuffer(seed)){
-    throw new Error('not buffers!');
-  }
+  checkArguments({seed});
   var seedPtr      = Module._malloc(32);
   var seedBuf      = new Uint8Array(Module.HEAPU8.buffer, seedPtr, 32);
   var publicKeyPtr = Module._malloc(32);
@@ -31,9 +65,8 @@ exports.createKeyPair = function(seed) {
 };
 
 exports.sign = function(message, publicKey, secretKey){
-  if(!Buffer.isBuffer(message) || !Buffer.isBuffer(publicKey) || !Buffer.isBuffer(secretKey)){
-    throw new Error('not buffers!');
-  }
+  if ('string' === typeof message) message = Buffer.from(message);
+  checkArguments({message,publicKey,secretKey});
   var messageLen = message.length;
   var messageArrPtr = Module._malloc(messageLen);
   var messageArr = new Uint8Array(Module.HEAPU8.buffer, messageArrPtr, messageLen);
@@ -54,23 +87,22 @@ exports.sign = function(message, publicKey, secretKey){
   return new Buffer(sig);
 };
 
-exports.verify = function(sig, message, publicKey){
-  if(!Buffer.isBuffer(message) || !Buffer.isBuffer(sig) || !Buffer.isBuffer(publicKey)){
-    throw new Error('not buffers!');
-  }
+exports.verify = function(signature, message, publicKey){
+  if ('string' === typeof message) message = Buffer.from(message);
+  checkArguments({signature,message,publicKey});
   var messageLen = message.length;
   var messageArrPtr = Module._malloc(messageLen);
   var messageArr = new Uint8Array(Module.HEAPU8.buffer, messageArrPtr, messageLen);
-  var sigArrPtr = Module._malloc(64);
-  var sigArr = new Uint8Array(Module.HEAPU8.buffer, sigArrPtr, 64);
+  var signatureArrPtr = Module._malloc(64);
+  var signatureArr = new Uint8Array(Module.HEAPU8.buffer, signatureArrPtr, 64);
   var publicKeyArrPtr = Module._malloc(32);
   var publicKeyArr = new Uint8Array(Module.HEAPU8.buffer, publicKeyArrPtr, 32);
   messageArr.set(message);
-  sigArr.set(sig);
+  signatureArr.set(signature);
   publicKeyArr.set(publicKey);
-  var res =  Module._verify(sigArrPtr, messageArrPtr, messageLen, publicKeyArrPtr) === 1;
+  var res =  Module._verify(signatureArrPtr, messageArrPtr, messageLen, publicKeyArrPtr) === 1;
   Module._free(messageArrPtr);
-  Module._free(sigArrPtr);
+  Module._free(signatureArrPtr);
   Module._free(publicKeyArrPtr);
   return res;
 };
