6a53cb79aea968226d90fdcddeda2e6682666fc9
finwo <finwo@pm.me>
2020-08-10 11:35:16 +0200

    Fixed test

--- test.js
+++ test.js
@@ -122,8 +122,8 @@ test('Key Exchange',async t => {
   const Bob     = await lib.createKeyPair(BobSeed);
   const Charlie = await lib.createKeyPair(CharlieSeed);
 
-  const AliceBobSecret = await lib.key_exchange(Alice.publicKey, Bob.secretKey);
-  const BobAliceSecret = await lib.key_exchange(Bob.publicKey, Alice.secretKey);
+  const AliceBobSecret = await lib.keyExchange(Alice.publicKey, Bob.secretKey);
+  const BobAliceSecret = await lib.keyExchange(Bob.publicKey, Alice.secretKey);
 
   t.ok(AliceBobSecret.equals(BobAliceSecret), 'Alice and Bob generate the same shared secret');
 });
